7aaa78b29e83baae38e597b3a052951c68fb5f82
chenbenqing <chenbenqing@talkweb.com.cn>
2021-03-31 17:10:03 +0800

    fix

--- jd_blueCoin.js
+++ jd_blueCoin.js
@@ -25,7 +25,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 let allMessage = '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let coinToBeans = $.getdata('coinToBeans') || 20; //兑换多少数量的京豆（20或者1000），0表示不兑换，默认兑换20京豆，如需兑换把0改成20或者1000，或者'商品名称'(商品名称放到单引号内)即可
+let coinToBeans = $.getdata('coinToBeans') || 20; //兑换多少数量的京豆（20或者1000），0表示不兑换，默认不兑换京豆，如需兑换把0改成20或者1000，或者'商品名称'(商品名称放到单引号内)即可
 let jdNotify = false;//是否开启静默运行，默认false关闭(即:奖品兑换成功后会发出通知提示)
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
@@ -159,6 +159,7 @@ async function PrizeIndex() {
       }
     } else {
       //自定义输入兑换
+      console.log(`\n\n温馨提示：需兑换商品的名称设置请尽量与其他商品有区分度，否则可能会兑换成其他类似商品\n\n`)
       let prizeId = '', i;
       for (let index = 0; index < prizeList.length; index ++) {
         if (prizeList[index].title.indexOf(coinToBeans) > -1) {
@@ -191,7 +192,7 @@ async function PrizeIndex() {
           $.beanerr = `兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`;
         }
       } else {
-        console.log(`奖品兑换列表[${coinToBeans}]已下架，请检查APP是否存在此商品，如存在请检查您的输入是否正确`);
+        console.log(`奖品兑换列表[${coinToBeans}]已下架，请检查活动页面是否存在此商品，如存在请检查您的输入是否正确`);
         $.beanerr = `奖品兑换列表[${coinToBeans}]已下架`;
       }
     }
